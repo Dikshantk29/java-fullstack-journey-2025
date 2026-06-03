0da19b830e81334661d12d8d16a669f56bd183a6
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2026-06-03 07:03:13 +0530

    implement encapsulation with BankAccount class and balance management

--- src/challenege_60_Days_Interview_pre/day_1/OOPs.java
+++ src/challenege_60_Days_Interview_pre/day_1/OOPs.java
@@ -1,4 +1,34 @@
 package challenege_60_Days_Interview_pre.day_1;
 
 public class OOPs {
+    public static void main(String[] args) {
+        //Encapsulation
+        BankAccount acc = new BankAccount();
+        acc.setBalance(1000);
+        System.out.println(acc.getBalance());
+        acc.setBalance(2000);
+        System.out.println(acc.getBalance());
+
+        //
+
+    }
+}
+
+class BankAccount{
+
+    private double balance;//data hiding
+
+    //getter
+    public double getBalance(){
+        return balance;
+    }
+
+    //setter + validation
+    public void setBalance(double amount){
+        if(amount > 0){
+            balance += amount;
+        }
+    }
+
+
 }
